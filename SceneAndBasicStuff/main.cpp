418b179736adec0de92dde5ce0ad111117f965c0
AlexVeraPons <Alexandroverapons@gmail.com>
2023-10-16 12:15:55 +0200

    added new main

--- SceneAndBasicStuff/main.cpp
+++ SceneAndBasicStuff/main.cpp
@@ -1,28 +1,61 @@
-#include <iostream>
-#include <string>
 #include <SFML/Graphics.hpp>
-#include <vector>
 
+#include <iostream>
+#include "scene.hpp"
+#include "spriteObject.hpp"
+#include "sceneHandler.hpp"
 
-const int WINDOW_SIZE_X= 800;
-const int WINDOW_SIZE_Y = 800;
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML with Scenes!");
+    Scene scene1("scene01");
 
-	sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y), "BasicScene");
+    SpriteObject sprite1("soldierSprite", "head.png");
+    sprite1.setPosition(sf::Vector2f(70.0f, 70.0f));
+    sprite1.setScale(sf::Vector2f(5.0f, 5.0f));
+    scene1.addGameObject(sprite1);
 
-	while (window.isOpen()) {
-		sf::Event event;
+    Scene scene2("scene02");
 
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+    SpriteObject sprite2("soldierSprite", "head_hurt.png");
+    sprite2.setPosition(sf::Vector2f(70.0f, 70.0f));
+    sprite2.setScale(sf::Vector2f(5.0f, 5.0f));
+    scene2.addGameObject(sprite2);
 
-		window.clear();
+    SceneHandler handler;
+    handler.addScene(scene1);
+    handler.addScene(scene2);
 
-		window.display();
-	}
+    int counter = 0;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyReleased) {
+                if (event.key.code == sf::Keyboard::A) {
+                    if (counter == 0) {
+                        handler.stackScene("scene02");
+                        counter++;
+                    }
+                    else {
+                        handler.popScene();
+                        counter--;
+                    }
+                }
+            }
+        }
+        window.clear();
+        handler.update();
+        handler.render(window);
+        window.display();
+    }
 
-	return 0;
+    return 0;
 }
+
+
+
+
+
