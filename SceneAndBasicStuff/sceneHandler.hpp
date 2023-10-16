d77e94c2b566fa408b299de20b5e022e03f8984c
AlexVeraPons <Alexandroverapons@gmail.com>
2023-10-16 12:15:05 +0200

    Added Basic code (not mine)

---
+++ SceneAndBasicStuff/sceneHandler.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <map>
+#include <stack>
+#include "scene.hpp"
+
+class SceneHandler {
+private:
+    std::map<std::string, Scene*> scenes;
+    std::stack<Scene*> scenesStack;
+public:
+    SceneHandler();
+    ~SceneHandler();
+public:
+    void render(sf::RenderWindow& window) const; // const because it doesn't change the object
+    void update();
+
+    void addScene(Scene& scene);
+    void stackScene(std::string sceneName); // Stack is to add an element to the top of the stack
+    void popScene(); // Pop is to remove the top element of the stack
+};
